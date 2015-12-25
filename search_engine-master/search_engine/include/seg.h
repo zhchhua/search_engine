a8b98e58cca7feba95b4f807502af2d656597229
zch <467883442@qq.com>
2015-12-25 23:20:36 +0800

    search_engine-master

---
+++ search_engine-master/search_engine/include/seg.h
@@ -0,0 +1,9 @@
+#define LOGGER_LEVEL LL_WARN
+#include "/home/zch/C++/text/program/search_engine/data/cppjieba/src/MPSegment.hpp"
+#include "/home/zch/C++/text/program/search_engine/data/cppjieba/src/HMMSegment.hpp"
+#include "/home/zch/C++/text/program/search_engine/data/cppjieba/src/MixSegment.hpp"
+using namespace CppJieba;
+const char * const JIEBA_DICT_FILE = "/home/zch/C++/text/program/search_engine/data/cppjieba/dict/jieba.dict.utf8";
+const char * const HMM_DICT_FILE = "/home/zch/C++/text/program/search_engine/data/cppjieba/dict/hmm_model.utf8";
+const char * const USER_DICT_FILE = "/home/zch/C++/text/program/search_engine/data/cppjieba/dict/user.dict.utf8";
+
